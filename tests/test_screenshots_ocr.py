b6f246cef31cbf538122b37cbb96834c03673793
Joe <jgliniecki@gmail.com>
2023-06-05 08:19:37 -0500

    Add another ocr example

--- tests/test_screenshots_ocr.py
+++ tests/test_screenshots_ocr.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 # For OCR, install packages: pillow, pytesseract
 from PIL import Image
 from pytesseract import pytesseract
+import easyocr
 
 import time
 
@@ -31,7 +32,7 @@ def test_page_screenshot():
 # Install Tesseract
 # https://tesseract-ocr.github.io/tessdoc/Installation.html
 # Mac: brew install tesseract
-def test_get_text_in_screenshot():
+def test_get_text_in_screenshot_tesseract():
     driver = webdriver.Chrome()
     driver.get("https://bonigarcia.dev/selenium-webdriver-java/web-form.html")
 
@@ -52,7 +53,30 @@ def test_get_text_in_screenshot():
 
     # Extract text from image
     text = pytesseract.image_to_string(img)
-    print("\n\n############### Start of text extracted from screenshot ####################")
+    print("\n\n############### Start of tesseract text extracted from screenshot ####################")
     print(text)
-    print("############### End of text extracted from screenshot ####################")
+    print("############### End of tesseract text extracted from screenshot ####################")
+    target_text = "Practice"
+    assert target_text in text, f"Could not find {target_text} in screenshot"
 
+def test_get_text_in_screenshot_easyocr():
+    driver = webdriver.Chrome()
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/web-form.html")
+
+    # Get screenshot
+    screenshot_file = f"./screenshots/screenshot_to_ocr_{time.strftime('%Y-%m-%d_%H%M%S')}.png"
+    driver.save_screenshot(screenshot_file)
+
+    # Search for text in screenshot
+    reader = easyocr.Reader(['en']) # Can read multiple languages, we're just doing English
+    result_array = reader.readtext(screenshot_file, detail = 0) # detail = 0 simplifies output, otherwise you get text box coordinates [x,y], text and model confident level for each piece of text
+
+    print("\n\n############### Start of easyocr text extracted from screenshot ####################")
+    print(result_array)
+    print("############### End of easyocr text extracted from screenshot ####################")
+
+    target_text = "Practice"
+    for phrase in result_array:
+        if target_text in phrase:
+            return
+    assert False, f"Could not find text '{target_text}' in screenshot"
