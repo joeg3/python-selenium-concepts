51819db3a5a007c3e5ddd7d82fdf0294e961269b
Joe <jgliniecki@gmail.com>
2023-10-03 13:51:50 -0500

    Have test cases use fixture

--- tests/test_locate_basics.py
+++ tests/test_locate_basics.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.relative_locator import locate_with
 
 """ There are eight basic location strategies:
     tag name, link text, partial link text, name, id, class name, css selector, xpath
@@ -13,38 +14,22 @@ from selenium.webdriver.common.by import By
     
 """
 
-def test_locate_by_html_tag_name():
+def test_locate_by_html_tag_name(web_form_page):
     """ Locate by HTML tag name (a, p, div, etc.) """
     
-    base_url = "https://bonigarcia.dev/selenium-webdriver-java"
-    url = f"{base_url}/web-form.html"
-
-    driver = webdriver.Chrome()
-    driver.get(url)
-
-    text_area = driver.find_element(By.TAG_NAME, "textarea")
+    text_area = web_form_page.find_element(By.TAG_NAME, "textarea")
     assert text_area.get_dom_attribute("rows") == "3"
 
-def test_locate_by_name():
+def test_locate_by_name(web_form_page):
     """ Locate by the element 'name' attribute """
-    base_url = "https://bonigarcia.dev/selenium-webdriver-java"
-    url = f"{base_url}/web-form.html"
-
-    driver = webdriver.Chrome()
-    driver.get(url)
-
-    element = driver.find_element(By.NAME, "my-text")
+    
+    element = web_form_page.find_element(By.NAME, "my-text")
     assert element.is_enabled() # Assert element is enabled (user can type in it)
 
-def test_locate_by_id():
+def test_locate_by_id(web_form_page):
     """ Locate by the element 'id' attribute """
-    base_url = "https://bonigarcia.dev/selenium-webdriver-java"
-    url = f"{base_url}/web-form.html"
 
-    driver = webdriver.Chrome()
-    driver.get(url)
-
-    element = driver.find_element(By.ID, "my-text-id")
+    element = web_form_page.find_element(By.ID, "my-text-id")
 
     # The 'type' attribute is standard, so it's also a property in the DOM
     assert element.get_attribute("type") == "text"
@@ -56,60 +41,21 @@ def test_locate_by_id():
     assert element.get_dom_attribute("myprop") == "myvalue"
     assert element.get_property("myprop") == None
 
-def test_locate_by_class_name():
+def test_locate_by_class_name(web_form_page):
     """ Locate by the element 'class' attribute """
-    base_url = "https://bonigarcia.dev/selenium-webdriver-java"
-    url = f"{base_url}/web-form.html"
-
-    driver = webdriver.Chrome()
-    driver.get(url)
-
-    elements = driver.find_elements(By.CLASS_NAME, "form-control")
+    
+    elements = web_form_page.find_elements(By.CLASS_NAME, "form-control")
     assert len(elements) > 0
     assert elements[0].get_attribute("name") == "my-text" # Assert first element found by class is same as input text located earlier
 
-def test_locate_by_link_text():
+def test_locate_by_link_text(web_form_page):
     """ Locate by the link text and partial link text """
-    base_url = "https://bonigarcia.dev/selenium-webdriver-java"
-    url = f"{base_url}/web-form.html"
 
-    driver = webdriver.Chrome()
-    driver.get(url)
-
-    link_by_text = driver.find_element(By.LINK_TEXT, "Return to index")
+    link_by_text = web_form_page.find_element(By.LINK_TEXT, "Return to index")
     assert link_by_text.tag_name == "a"
     assert link_by_text.value_of_css_property("cursor") == "pointer"
 
-    link_by_partial_text = driver.find_element(By.PARTIAL_LINK_TEXT, "index")
+    link_by_partial_text = web_form_page.find_element(By.PARTIAL_LINK_TEXT, "index")
     assert link_by_text.location == link_by_partial_text.location
     assert link_by_text.rect == link_by_partial_text.rect
-
-
-
-
-
-
-
-def test_locate_by_xpath_selector():
-    """ Locate by xpath selector """
-    base_url = "https://bonigarcia.dev/selenium-webdriver-java"
-    url = f"{base_url}/web-form.html"
-
-    driver = webdriver.Chrome()
-    driver.get(url)
-
-    # Syntax for XPath queries: //tagname[@attribute='value']
-    # Element we are locating: <input type="hidden" name="my-hidden">
-    hidden_element = driver.find_element(By.XPATH, "//input[@type='hidden']")
-    assert hidden_element.is_displayed() == False
-
-    # More elements we are locating:
-    # <input class="form-check-input" type="radio" name="my-radio" id="my-radio-1" checked>
-    # <input class="form-check-input" type="radio" name="my-radio" id="my-radio-2">
-    r1 = driver.find_element(By.XPATH, "//*[@type='radio' and @checked]")
-    assert r1.get_attribute("id") == "my-radio-1"
-    assert r1.is_selected()
-
-    r2 = driver.find_element(By.XPATH, "//*[@type='radio' and not(@checked)]")
-    assert r2.get_attribute("id") == "my-radio-2"
-    assert not r2.is_selected()
+    
